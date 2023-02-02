201177bc825d5f49bd20111da078022d9b206d20
HunterKF <95839328+HunterKF@users.noreply.github.com>
2023-02-02 18:16:53 +0900

    Tried out shimmer effect and auto resized text

--- app/src/main/java/com/example/uiplayground/MainActivity.kt
+++ app/src/main/java/com/example/uiplayground/MainActivity.kt
@@ -3,24 +3,83 @@ package com.example.uiplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.uiplayground.presentation.FitnessTracker
 import com.example.uiplayground.presentation.MindfullnessApp.AllTime
 import com.example.uiplayground.presentation.MindfullnessApp.Statistics
+import com.example.uiplayground.presentation.ShimmerListItem
+import com.example.uiplayground.presentation.autoresizetext.AutoResizedText
 import com.example.uiplayground.ui.theme.UiPlaygroundTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             UiPlaygroundTheme {
-                AllTime()
+                var isLoading by remember {
+                    mutableStateOf(true)
+                }
+                LaunchedEffect(key1 = true, block = {
+                    delay(2000)
+                    isLoading = false
+                })
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .background(Color.LightGray)
+                    ) {
+                        AutoResizedText(
+                            text = "Plastic world",
+                            style = MaterialTheme.typography.h1
+                        )
+                    }
+                }
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    items(20) {
+                        ShimmerListItem(
+                            isLoading = isLoading,
+                            contentAfterLoading = {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(Color.LightGray)
+                                ) {
+                                    AutoResizedText(
+                                        text = "Hello world",
+                                        style = MaterialTheme.typography.h1
+                                    )
+                                }
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/uiplayground/presentation/MindfullnessApp/AllTime.kt
+++ app/src/main/java/com/example/uiplayground/presentation/MindfullnessApp/AllTime.kt
@@ -1,13 +1,16 @@
 package com.example.uiplayground.presentation.MindfullnessApp
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -17,12 +20,15 @@ import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.example.uiplayground.R
 import com.example.uiplayground.presentation.linearGradient
 import com.example.uiplayground.ui.theme.*
@@ -93,7 +99,40 @@ fun AllTime() {
             Text(
                 text = "This week"
             )
-            Chart()
+            Card(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(15.dp)),
+                backgroundColor = Black500
+            ) {
+                Column(
+                    modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)
+                ) {
+                    Row(Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(
+                            text = "Total time:",
+                            style = MaterialTheme.typography.h5
+                        )
+                        Text(
+                            text = "3h 34min",
+                            fontWeight = FontWeight.Bold,
+                            style = MaterialTheme.typography.h5
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Box(
+                        Modifier.height(300.dp)
+                    ) {
+
+                        Chart()
+                    }
+                }
+
+            }
+
 
         }
     }
@@ -182,19 +221,30 @@ fun Chart(
         "30 m",
         "15 m",
     )
+    val stats = listOf(
+        0.2f,
+        0.3f,
+        0.27f,
+        0.21f,
+        0.65f,
+        1f,
+        0.14f
+    )
+
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.White
-                .copy(0.9f))
     ) {
 
         Column(Modifier
             .fillMaxHeight()
-            .weight(0.1f), verticalArrangement = Arrangement.SpaceBetween) {
+            .weight(0.15f), verticalArrangement = Arrangement.SpaceBetween) {
             time.forEach {
-                Text(text = it)
+                Text(
+                    text = it,
+                    color = Color.LightGray.copy(0.5f)
+                )
             }
             Text("")
             Text("")
@@ -202,7 +252,7 @@ fun Chart(
 
 
         Box(Modifier
-            .weight(0.99f)
+            .weight(0.9f)
             .fillMaxSize()) {
             Column(
                 Modifier
@@ -210,16 +260,29 @@ fun Chart(
                     .offset(y = 14.dp),
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
-                time.forEach {
-                    Divider()
+                time.forEach { _ ->
+                    Divider(Modifier.height(2.dp),
+                        color = Color.LightGray.copy(0.5f))
                 }
-                Divider()
+                Divider(Modifier.height(2.dp),
+                    color = Color.LightGray.copy(0.5f))
             }
+            var size by remember { mutableStateOf(IntSize.Zero) }
             Box(Modifier
                 .fillMaxWidth()
-                .height(240.dp)
-                .border(2.dp, Color.Red)) {
-                ChartBar(height = 0.45f)
+                .onSizeChanged {
+                    size = it
+                }
+                .height(240.dp)) {
+                Row(Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 0.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    val columnWidth = (size.width.dp / 7)
+                    stats.forEach {
+                        ChartBar(height = it, size = size, columnWidth = columnWidth)
+                    }
+                }
             }
             Spacer(Modifier.height(10.dp))
             Row(Modifier
@@ -228,7 +291,12 @@ fun Chart(
                 .padding(top = 12.dp, bottom = 40.dp),
                 horizontalArrangement = Arrangement.SpaceBetween) {
                 days.forEach {
-                    Text(it)
+                    Box(Modifier.border(2.dp, Color.Red)) {
+                        Text(it,
+                            textAlign = TextAlign.Center,
+
+                            color = Color.LightGray.copy(0.5f))
+                    }
                 }
             }
 
@@ -240,30 +308,58 @@ fun Chart(
 fun ChartBar(
     width: Dp = 8.dp,
     color: Color = NeonTeal,
-    height: Float = 0f
+    height: Float = 0.5f,
+    columnWidth: Dp,
+    size: IntSize,
 ) {
+    val context = LocalContext.current
     val spacerHeight = 1f - height
-    Spacer(Modifier.fillMaxHeight(spacerHeight))
-    Box(
-        modifier = Modifier
-            .width(width)
-            .fillMaxHeight(height)
-            .background(
-                Brush.linearGradient(
-                    colors = listOf(
-                        color,
-                        Color.Transparent
+    Column(Modifier
+        .fillMaxHeight()
+        .width(40.dp)
+        .border(2.dp, Color.Blue),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        if (spacerHeight in 0.1f..1f) {
+            Box(Modifier
+                .width(width)
+                .weight(spacerHeight)
+                .fillMaxHeight()
+            )
+//                .border(2.dp, Color.Green))
+        }
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(100))
+                .weight(height)
+                .width(width)
+                .fillMaxHeight(0.5f)
+                .background(
+                    Brush.linearGradient(
+                        0.1f to color,
+                        0.4f to color,
+                        0.6f to color.copy(0.5f),
+                        0.8f to Color.Transparent,
+                        angleInDegrees = 270f
                     )
                 )
-            )
-    )
+//                .border(2.dp, Color.Blue)
+                .clickable {
+                    Toast
+                        .makeText(context,
+                            "Value of spacerHeight: ${spacerHeight}",
+                            Toast.LENGTH_SHORT)
+                        .show()
+                }
+        )
+    }
+
 }
 
 
 @Preview()
 @Composable
 fun Preview2() {
-    Box(Modifier.height(300.dp)){
+    Box(Modifier.height(300.dp)) {
         Chart()
     }
 
